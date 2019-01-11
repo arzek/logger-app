69aa1f0be5076021b7870ac96846803ebe30f063
Artem <arzek2015@gmail.com>
2019-01-11 18:03:19 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactTable from 'react-table'
+import "react-table/react-table.css";
+
+import axios from 'axios';
+
+const columns = [
+  {
+    Header: 'ID',
+    accessor: 'id' // String-based value accessors!
+  },
+  {
+    Header: 'Name',
+    accessor: 'name' // String-based value accessors!
+  }, {
+    Header: 'Status',
+    accessor: 'status',
+    Cell: props => props.value ? 'Success' : 'Error'
+  }
+];
 
 class App extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      axios.get('http://localhost:3001/loggs').then(res => {
+        this.setState({
+          data: res.data
+        })
+      })
+    }, 500);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <ReactTable
+      data={this.state.data}
+      columns={columns}
+    />
   }
 }
 
